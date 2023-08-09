97d3ab72ef54898626ae1d4e62f6b27fd4a519f1
hasan cimcik <cimcikhasan06@gmail.com>
2023-08-09 20:17:32 +0300

    Initial Commit

---
+++ StateSwiftUI/StateSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateSwiftUIApp.swift
+//  StateSwiftUI
+//
+//  Created by Hasan PC on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct StateSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
